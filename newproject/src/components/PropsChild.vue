8099fea6987ddf87f8eef85debd9522b633546ff
zhihao.sun <zhihao.sun@xrc-inc.com>
2018-03-09 17:08:19 +0800

    update

--- newproject/src/components/PropsChild.vue
+++ newproject/src/components/PropsChild.vue
@@ -1,5 +1,8 @@
 <template>
-  <p>Props <span class="red">{{ msg }}</span></p>
+  <div class="propsChild">
+    <p>Props <span class="red">{{ msg }}</span></p>
+    <p>This part is rendered by the 'PropsChild' component.</p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -13,9 +16,12 @@
   }
 </script>
 
-<style lang="less">
-.red {
-  color: red;
+<style scoped lang="less">
+.propsChild {
+  border: 1px solid red;
+  .red {
+    color: red;
+  }
 }
 </style>
 
